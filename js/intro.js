5e7e18deef82aaee6d6520b2e4f79066c90dcb33
rabiulislam993 <rabiulislam993@live.com>
2017-12-06 22:40:38 +0600

    modified intro.js

--- js/intro.js
+++ js/intro.js
@@ -17,34 +17,7 @@
 			dropRadius: 20,
 			perturbance: 0.01,
 		});
-		$('.purchase').ripples({
-			resolution: 512,
-			dropRadius: 20,
-			perturbance: 0.04,
-		});
-
 		
-		
-		
-    }); /*  End of document.ready */
-	
-
-	/*==============================================
-	  Scroll to top
-	 ===============================================*/
-	$('.scrollToTop').on('click',(function(){
-		$('html, body').animate({scrollTop : 0},600);
-		return false;
-	}));
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+    }); 
 	
 })(jQuery);
